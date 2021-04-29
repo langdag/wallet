67fc0d65552316c53cd09e956be382bbe46052ea
langdag <lucky10cents@gmail.com>
2021-04-29 19:51:15 +0300

    fix routes/transactions

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :users
   resources :wallets, only: :show do
-    post '/deposit', to: 'wallets#new_deposit'
-    post '/withdraw', to: 'wallets#new_withdraw'
-    post '/transfer', to: 'wallets#new_transfer'
+    get '/new/deposit', to: 'wallets#new_deposit'
+    get '/new/withdraw', to: 'wallets#new_withdraw'
+    get '/new/transfer', to: 'wallets#new_transfer'
+    post 'deposit', to: 'wallets#deposit'
+    post 'withdraw', to: 'wallets#withdraw'
+    post 'transfer', to: 'wallets#transfer'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
