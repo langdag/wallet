66af4cc423e45a3a526a16e61e37bbeb9e867425
langdag <lucky10cents@gmail.com>
2021-04-29 19:42:56 +0300

    create view for transactions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :teams
   root 'home#index'
   resources :users
-  resources :wallets, only: :show
+  resources :wallets, only: :show do
+    post '/deposit', to: 'wallets#new_deposit'
+    post '/withdraw', to: 'wallets#new_withdraw'
+    post '/transfer', to: 'wallets#new_transfer'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
